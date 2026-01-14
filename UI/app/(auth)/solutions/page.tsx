27a1a0291be0fbefddb6e60294931490976d9935
Franklin Filho <franklin.filho@heat.com>
2026-01-14 17:36:46 -0500

    feat: refactor loading state management and navigation in SolutionsDefaultPage

--- UI/app/(auth)/solutions/page.tsx
+++ UI/app/(auth)/solutions/page.tsx
@@ -2,14 +2,26 @@
 
 import { useAuth } from "@/hooks/useAuth";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function SolutionsDefaultPage() {
     // loading state
     const {user, loading} = useAuth();
     const router = useRouter();
 
-    if (loading) {
-        return (
+    useEffect(() => {
+        const navigate = async () => {
+            if (!loading) {
+                if (user) {
+                    await router.push("/solutions/dashboard");
+                } else {
+                    await router.push("/login");
+                }
+            }
+        };
+        navigate();
+    }, [loading, user, router]);
+    return (
             <div className="flex items-center justify-center min-h-screen">
                 <div className="text-center">
                     <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-gray-900 mx-auto"></div>
@@ -17,13 +29,4 @@ export default function SolutionsDefaultPage() {
                 </div>
             </div>
         );
-    }
-    else{
-        if(user){
-            router.push("/solutions/dashboard");
-        }
-        else{
-            router.push("/login");
-        }
-    }
 }
